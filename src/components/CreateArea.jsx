691e4fbc5c8563ecc5126cf7ab8358d369cbaa3e
Manthan <manthan.asher@gmail.com>
2020-05-22 14:05:51 +0530

    Final keeper app using react

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [zoom, setZoom] = useState(false);
+
+  function handleZoom() {
+    setZoom(true);
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {zoom && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )} 
+
+        <textarea
+          onClick={handleZoom}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={zoom ? 3 : 1}
+        />
+        <Zoom in={zoom}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
